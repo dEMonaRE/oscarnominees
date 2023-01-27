5b5f59ecc8960f1fdb5f7061e1ee25a0585c8c59
Emre Zorlu <ahmet@emrezorlu.com>
2023-01-27 21:09:49 +0300

    initial

---
+++ src/test/java/com/emrezorlu/app/demo/oscarnominees/OscarnomineesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emrezorlu.app.demo.oscarnominees;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OscarnomineesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
