85d6a87e4cdf0373660242b69a26ea460b38ed64
Emre Zorlu <ahmet@emrezorlu.com>
2023-01-28 14:56:38 +0300

    request update

--- src/main/java/com/emrezorlu/app/demo/oscarnominees/oscar/datamodel/RequestSaveSelectedNominees.java
+++ src/main/java/com/emrezorlu/app/demo/oscarnominees/oscar/datamodel/RequestSaveSelectedNominees.java
@@ -1,14 +1,21 @@
 package com.emrezorlu.app.demo.oscarnominees.oscar.datamodel;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.Map;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class RequestSaveSelectedNominees {
-    
+    @NotBlank(message = "Source currency should not be null")
+    private String username;
+    //degisebilir categoryname-selected nomineeID
+    private Map<String, String> selectedNominees;
+
 }
